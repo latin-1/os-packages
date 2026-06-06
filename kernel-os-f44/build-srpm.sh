d88acc1598a48e1446e2bcfc080a2a737bde98e0
Hikari Hayashi <rev.hikari@gmail.com>
2026-06-06 19:59:43 +0800

    Add a custom kernel

---
+++ kernel-os-f44/build-srpm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -euo pipefail
+
+OUTDIR="$1"
+source ./version
+
+fedpkg clone --anonymous --branch "$BRANCH" kernel
+cd kernel
+git switch "$COMMIT"
+
+fedpkg sources
+
+sed --in-place "/^# define buildid /c\%define buildid .os" ./kernel.spec
+for patch in ../patches/*.patch; do
+  cat "$patch" >> ./linux-kernel-test.patch
+done
+
+fedpkg srpm
+mv ./*.src.rpm "$OUTDIR/"
